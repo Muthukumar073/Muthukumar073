5ff91efca9589aedd9ded3d31bc5677d2c5462b7
Muthukumar073 <136782181+Muthukumar073@users.noreply.github.com>
2024-03-03 20:23:14 +0530

    Delete Factorial Number.py

--- Factorial Number.py
+++
@@ -1,9 +0,0 @@
-Factorial Number
-def fact(x):
-    if x == 1 or x == 0:
-        return 1
-    else:
-        return x*fact(x-1)
-
-num=int(input("Enter a Number :"))
-print(f"The Factorial of {num} is {fact(num)}")
