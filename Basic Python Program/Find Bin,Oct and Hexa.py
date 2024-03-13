063b113f46605abd52ec6346640bc04bbd656b28
Muthukumar073 <136782181+Muthukumar073@users.noreply.github.com>
2024-03-13 14:31:19 +0530

    Add files via upload

---
+++ Basic Python Program/Find Bin,Oct and Hexa.py
@@ -0,0 +1,4 @@
+num=int(input("Enter the Number:"))
+print(f"Binary number of {num} is :{bin(num)}")
+print(f"Octal number of {num} is :{oct(num)}")
+print(f"Hexadecimal number of {num} is :{hex(num)}")
