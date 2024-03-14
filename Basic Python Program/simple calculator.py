017e8f48c24edb190b03c73deeccb52772978793
Muthukumar073 <136782181+Muthukumar073@users.noreply.github.com>
2024-03-14 14:21:32 +0530

    Add files via upload

---
+++ Basic Python Program/simple calculator.py
@@ -0,0 +1,52 @@
+def Add(a,b):
+    return a+b;
+def Sub(a,b):
+    return a-b;
+def Mul(a,b):
+    return a*b;
+def Div(a,b):
+    return a/b;
+
+print("For Addition choose :1")
+print("For Subtraction choose :2")
+print("For Multiplication choose :3")
+print("For Division choose :4")
+choice=int(input("Enter the choice :"))
+num1=int(input("Enter the Number1 :"))
+num2=int(input("Enter the Number2 :"))
+if choice==1:
+    print("Addition of Two Numbers is :",Add(num1,num2))
+elif choice==2:
+    print("Subtraction of Two Numbers is :",Sub(num1,num2))
+elif choice==3:
+    print("Multiplication of Two Numbers is :",Mul(num1,num2))
+elif choice==4:
+    print("Division of Two Numbers is :",div(num1,num2))
+else:
+    print("Invalid Option")
+def Add(a,b):
+    return a+b;
+def Sub(a,b):
+    return a-b;
+def Mul(a,b):
+    return a*b;
+def Div(a,b):
+    return a/b;
+
+print("For Addition choose :1")
+print("For Subtraction choose :2")
+print("For Multiplication choose :3")
+print("For Division choose :4")
+choice=int(input("Enter the choice :"))
+num1=int(input("Enter the Number1 :"))
+num2=int(input("Enter the Number2 :"))
+if choice==1:
+    print("Addition of Two Numbers is :",Add(num1,num2))
+elif choice==2:
+    print("Subtraction of Two Numbers is :",Sub(num1,num2))
+elif choice==3:
+    print("Multiplication of Two Numbers is :",Mul(num1,num2))
+elif choice==4:
+    print("Division of Two Numbers is :",div(num1,num2))
+else:
+    print("Invalid Option")
